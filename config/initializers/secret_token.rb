b6d5f72c8bf3f827741bdb27d8b7f9d17583706b
Nicholas Greenfield <Greenie0506@gmail.com>
2012-03-09 19:06:15 -0500

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DodgeDate::Application.config.secret_token = '93d4f1190e4b0e8009ac79f03d3cf5afb423ca3de784dc65393fa5ab59ceefb28b383adc9321343f898e09c5da6319133f0d304e101022e384310f334e076c50'
