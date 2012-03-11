0912da19669ef2236e7288a69cf5cef6331c3295
Nicholas Greenfield <Greenie0506@gmail.com>
2012-03-11 16:47:08 -0400

    moved twilio variables outside of initialize

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,11 +1,10 @@
 class Call
   attr_accessor :number
+  account_sid = ENV['TWILIO_SID']
+  auth_token = ENV['TWILIO_TOKEN']
 
   def initialize(number)
     @number = number
-
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
