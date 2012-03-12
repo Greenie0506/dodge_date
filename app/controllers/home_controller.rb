a7ab41d70b2e889aa1ffc8569d51e87e5aaf855d
Nicholas Greenfield <Greenie0506@gmail.com>
2012-03-12 00:10:24 -0400

    layout none

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,6 @@ class HomeController < ApplicationController
   end
 
   def voice
-    render :file => 'public/voice.txt', :content_type => 'text/xml'
+    render :file => 'public/voice.txt', :content_type => 'text/xml', :layout => false
   end
 end
