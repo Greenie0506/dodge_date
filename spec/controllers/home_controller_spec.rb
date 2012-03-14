74709618be7d618c7b65a343ed5b8b97db2db731
Nicholas Greenfield <Greenie0506@gmail.com>
2012-03-13 20:25:13 -0400

    Add test around voice action

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -10,11 +10,19 @@ describe HomeController do
 
   describe "call_me" do
     it "should instantiate a new call" do
-      post :call_me, {number: "18602686902"}
+      post :call_me, {number: "8602686902"}
 
       call = assigns :call
       call.should be
       call.number.should == "18602686902"
+    end
+  end
+
+  describe "voice" do
+    it "should render a file and have the content type of txt/xml with no layout" do
+      post :voice, :file => 'public/voice.txt', :content_type => 'txt/xml', :layout => false
+
+      response.should be_success
     end
   end
 end
