d81f2a7f4c4a8b36747a70e7500d8e3ada798971
Greenie0506 <greenie0506@gmail.com>
2012-06-13 22:10:56 -0400

    Typo

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -5,7 +5,5 @@ class CallController < ApplicationController
   def create
     @call = Call.create!(number: params[:number])
     @call.delay(run_at: Call::CALL_TIMES[params[:time]]).make_call
-
-    render text: "Your call has been placed"
   end
 end
